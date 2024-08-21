a394b137b82a0ee2b9a03f5fc688ed89e2883ae2
dhev390 <sayuragimhan5@gmail.com>
2024-08-21 12:46:21 +0530

    config.js

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
-};
+    ALIVE_IMG: process.env.ALIVE_IMG || '',
+    ALIVE_MSG: process.env.ALIVE_MSG || '🔒🔗💗° :|° ᥫ᭡-HEY.I AM MINZZA-MD.THIS TIME I AM ALIVE NOW ⚝',
+}
